68cbfa3b3e779d3f28f9dbbf9b1b9727a8bb5f81
Ani-8712 <114625811+Ani-8712@users.noreply.github.com>
2023-04-13 15:55:04 -0700

    plslslsls

--- src/main/java/frc/robot/Constants/Constants.java
+++ src/main/java/frc/robot/Constants/Constants.java
@@ -1,11 +1,15 @@
 // Copyright (c) FIRST and other WPILib contributors.
 package frc.robot.Constants;
 
+import edu.wpi.first.math.util.Units;
+
 public class Constants {
     public static final int leftPin = 1;
     public static final int rightPin = 2;
     public static final double kDegreesToTicks = (60 / 14) * 2048;
     public static final double kTicksToDegrees = (1 / 2048) * (14 / 60);
-    public static final double kTickstoMeters = 0;// CHANGE TO REAL CONVERSION
-    public static final double kMetersToTicks = 0;// CHANGE TO REAL CONVERSION
+    public static final double kTickstoMeters = ((60 / 14) * 2048 * 360)
+            * (0.1 * (Units.inchesToMeters(4)) * 2 * Math.PI);
+    public static final double kMetersToTicks = 1
+            / (((60 / 14) * 2048 * 360) * (Units.inchesToMeters(4) * 2 * Math.PI));
 }
--- src/main/java/frc/robot/Constants/DriveConstants.java
+++ src/main/java/frc/robot/Constants/DriveConstants.java
@@ -3,24 +3,21 @@ package frc.robot.Constants;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
 
-
 public class DriveConstants {
     public static final TalonFX left = new TalonFX(Constants.leftPin);
     public static final TalonFX right = new TalonFX(Constants.rightPin);
 
     public static final TalonFXConfiguration kMasterConfig = new TalonFXConfiguration();
 
-
-
-    static{
-        //configures PIDF for kMasterConfig 
-        kMasterConfig.slot0.kP = 0;
+    static {
+        // configures PIDF for kMasterConfig
+        kMasterConfig.slot0.kP = 0.3;
         kMasterConfig.slot0.kI = 0;
-        kMasterConfig.slot0.kD = 0;
+        kMasterConfig.slot0.kD = 0.1;
         kMasterConfig.slot0.kF = 0;
-        
-        //applies config object to the motor
-        left.configAllSettings(kMasterConfig, 0);//CHANGE DELAY TO SMTHING MEANINGFULL
-        right.configAllSettings(kMasterConfig, 0);
+
+        // applies config object to the motor
+        left.configAllSettings(kMasterConfig, 500);// CHANGE DELAY TO SMTHING MEANINGFULL
+        right.configAllSettings(kMasterConfig, 500);
     }
 }
